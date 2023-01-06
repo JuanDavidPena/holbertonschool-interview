c3d051f5a4d4cbcb84b6deafa2530c12ebe9e629
JuanDavidPeña <j_david0915@hotmail.com>
2023-01-06 15:00:06 -0500

    add tasks

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -11,21 +11,21 @@
 */
 void heapify(int *array, size_t size, size_t calcsize, int idx)
 {
-    int root = idx;
-    int left = 2 * idx + 1;
-    int right = 2 * idx + 2;
-    int n = (int) size;
+  int root = idx;
+  int left = 2 * idx + 1;
+  int right = 2 * idx + 2;
+  int n = (int) size;
 
-    if (left < n && array[left] > array[root])
+  if (left < n && array[left] > array[root])
     root = left;
-    if (right < n && array[right] > array[root])
+  if (right < n && array[right] > array[root])
     root = right;
-    if (root != idx)
-    {
-        swap(array[idx], array[root]);
-        print_array(array, calcsize);
-        heapify(array, n, calcsize, root);
-    }
+  if (root != idx)
+  {
+    swap(array[idx], array[root]);
+    print_array(array, calcsize);
+    heapify(array, n, calcsize, root);
+  }
 }
 
 /**
@@ -36,13 +36,13 @@ void heapify(int *array, size_t size, size_t calcsize, int idx)
 */
 void heap_sort(int *array, size_t size)
 {
-    int i;
-    for (i = size / 2 - 1; i >= 0; i--)
+  int i;
+  for (i = size / 2 - 1; i >= 0; i--)
     heapify(array, size, size, i);
-    for (i = size - 1; i > 0; i--)
-    {
-        swap(array[0], array[i]);
-        print_array(array, size);
-        heapify(array, i, size, 0);
-    }
+  for (i = size - 1; i > 0; i--)
+  {
+    swap(array[0], array[i]);
+    print_array(array, size);
+    heapify(array, i, size, 0);
+  }
 }
